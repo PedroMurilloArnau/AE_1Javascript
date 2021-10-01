57f0d6a0d7021020014bf141f55631f9734747cc
PedroMurilloArnau <Pedro.marnau@gmail.com>
2021-10-01 14:33:32 +0200

    Añadimos array y media

---
+++ JS/array.js
@@ -0,0 +1,33 @@
+function recorarray(){
+let i
+let array1 = [i]
+for (i=0; i<10;i++){
+array1[i]=Math.round((Math.random() * 200))
+/*De la siguiente manera utilizamos Math.random()*200 para
+crear una serie de números aleatorios entre 0 y 200 y 
+utilizamos el metodo Math.round para redondear el resultado
+*/
+}
+console.log(array1)
+//De esta manera mostramos por consola nuestro resultado
+for(i=0; i<array1.length;i++){
+    if(array1[i]>100){
+    delete array1[i]
+    }
+}
+let e
+let arrayf = [e]
+arrayf = array1.filter(Number)
+/*Mediante el método filter podremos filtrar las casillas
+que poseen un número por tanto que esten comprendidas entre
+el 0 y el 100*/
+
+
+console.log('Nuestro array sera el siguiente = ' + arrayf)
+let sum = 0
+for (e=0; e<arrayf.length;e++){
+    sum = sum + arrayf[e]
+}
+let media=(sum/arrayf.length)
+console.log('La media sera de : ' + media)
+}
