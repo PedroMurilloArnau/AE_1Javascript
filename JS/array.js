ff329f911dcf6289d91bb833c545aa5b121c9fd7
PedroMurilloArnau <Pedro.marnau@gmail.com>
2021-10-01 14:49:09 +0200

    Arreglando codigo

--- JS/array.js
+++ JS/array.js
@@ -8,13 +8,18 @@ crear una serie de números aleatorios entre 0 y 200 y
 utilizamos el metodo Math.round para redondear el resultado
 */
 }
-console.log(array1)
+console.log('Este seria nuestro array sin filtrar = ' + array1)
+
 //De esta manera mostramos por consola nuestro resultado
+
 for(i=0; i<array1.length;i++){
     if(array1[i]>100){
     delete array1[i]
     }
 }
+/*mediante el metodo "delete" eliminamos las casillas con
+numeros comprendidos entre el 0 y el 100 */
+
 let e
 let arrayf = [e]
 arrayf = array1.filter(Number)
@@ -23,7 +28,7 @@ que poseen un número por tanto que esten comprendidas entre
 el 0 y el 100*/
 
 
-console.log('Nuestro array sera el siguiente = ' + arrayf)
+console.log("Nuestro array finalmente seria el siguiente = "  + arrayf)
 let sum = 0
 for (e=0; e<arrayf.length;e++){
     sum = sum + arrayf[e]
